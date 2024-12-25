e40b460f33d599ddca35e552c5586b8cc55f3194
YosaZiege <YosaZiege@gmail.com>
2024-12-25 22:22:50 +0100

    admin

--- migration/0001_create_tables.up.sql
+++ migration/0001_create_tables.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE subscriptions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    password VARCHAR(60) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     internet_limit VARCHAR(50) NOT NULL,
     call_minutes VARCHAR(50) NOT NULL,
@@ -8,16 +9,18 @@ CREATE TABLE subscriptions (
     international_minutes VARCHAR(50) NOT NULL,
     inwi_calls_unlimited BOOLEAN NOT NULL
 );
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(15) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) UNIQUE NOT NULL
 );
+
 CREATE TABLE payments (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    subscription_id INT REFERENCES subscriptions(id),
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,  -- Optional action to delete payments when the user is deleted
+    subscription_id INT REFERENCES subscriptions(id) ON DELETE CASCADE,  -- Optional action to delete payments when the subscription is deleted
     amount_paid DECIMAL(10, 2) NOT NULL,
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     months_paid INT NOT NULL,  
